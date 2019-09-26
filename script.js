82e2f6a8432fb220e4580e902858c041c9a94fff
byron baldwin <byronbaldwin@byrons-MacBook-Pro.local>
2019-09-26 14:44:05 -0500

    saving for backup

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ window.addEventListener('DOMContentLoaded', function() {
     let h3Liz = document.createElement('h3');
     let lizText = document.createTextNode('Liz');
     let lizPara = document.createElement('p');
-    let lizParaText = document.createTextNode('');
-
+    let lizParaText = document.createTextNode(+ n + ' line of code in the file,' , + n + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+    let lizElseText = document.createTextNode(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');
 
     let div2 = document.createElement('div');
     div2.className = 'friend';
@@ -22,7 +22,8 @@ window.addEventListener('DOMContentLoaded', function() {
     let h3Rod = document.createElement('h3');
     let rodText = document.createTextNode('Rod');
     let rodPara = document.createElement('p');
-    let rodParaText = document.createTextNode('');
+    let rodParaText = document.createTextNode(+ n + ' line of code in the file,' , + n + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+    let rodElseText = document.createTextNode(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');
 
     let div3 = document.createElement('div');
     div3.className = 'friend';
@@ -31,7 +32,8 @@ window.addEventListener('DOMContentLoaded', function() {
     let h3Alan = document.createElement('h3');
     let alanText = document.createTextNode('Alan');
     let alanPara = document.createElement('p');
-    let alanParaText = document.createTextNode('');
+    let alanParaText = document.createTextNode(+ n + ' line of code in the file,' , + n + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+    let alanElseText = document.createTextNode(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');
 
     let div4 = document.createElement('div');
     div4.className = 'friend';
@@ -40,8 +42,8 @@ window.addEventListener('DOMContentLoaded', function() {
     let h3Fain = document.createElement('h3');
     let fainText = document.createTextNode('Fain');
     let fainPara = document.createElement('p');
-    let fainParaText = document.createTextNode('');
-    
+    let fainParaText = document.createTextNode(+ n + ' line of code in the file,' , + n + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+    let fainElseText = document.createTextNode(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');
     
     let div5 = document.createElement('div');
     div5.className = 'friend';
@@ -50,63 +52,68 @@ window.addEventListener('DOMContentLoaded', function() {
     let h3Charles = document.createElement('h3');
     let charlesText = document.createTextNode('Charles');
     let charlesPara = document.createElement('p');
-    let charlesParaText = document.createTextNode('');
-
+    let charlesParaText = document.createTextNode(+ n + ' line of code in the file,' , + n + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+    let CharlesElseText = document.createTextNode(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');
     
-    header.appendChild(h1Text);
-    lizPara.appendChild(lizParaText);
-    h3Liz.appendChild(lizText);
-    h3Liz.appendChild(lizPara);
-    div1.appendChild(h3Liz);
-
-    header.appendChild(div1);
-    rodPara.appendChild(rodParaText);
-    h3Rod.appendChild(rodText);
-    h3Rod.appendChild(rodPara);
-    div2.appendChild(h3Rod);
-
-    header.appendChild(div2);
-    alanPara.appendChild(alanParaText);
-    h3Alan.appendChild(alanText);
-    h3Alan.appendChild(alanPara);
-    div3.appendChild(h3Alan);
-
-    header.appendChild(div3);
-    fainPara.appendChild(fainParaText);
-    h3Fain.appendChild(fainText);
-    h3Fain.appendChild(fainPara);
-    div4.appendChild(h3Fain);
-
-    header.appendChild(div4);
-    charlesPara.appendChild(charlesParaText);
-    h3Charles.appendChild(charlesText);
-    h3Charles.appendChild(charlesPara);
-    div5.appendChild(h3Charles);
-    header.appendChild(div5);
-    document.body.appendChild(header);
-
-
     let friends = ['Liz', 'Rod', 'Alan', 'Fain', 'Charles'];
-        
+    
     let n = 99;
 
-    let btn = document.getElementsByTagName('button');
+    let btn = document.getElementsByTagName('button')[0];
+
+        
+
 
 
     
     btn.addEventListener('click', function() {
+        header.appendChild(h1Text);
+        lizPara.appendChild(lizParaText);
+        h3Liz.appendChild(lizText);
+        h3Liz.appendChild(lizPara);
+        div1.appendChild(h3Liz);
+
+        header.appendChild(div1);
+        rodPara.appendChild(rodParaText);
+        h3Rod.appendChild(rodText);
+        h3Rod.appendChild(rodPara);
+        div2.appendChild(h3Rod);
+
+        header.appendChild(div2);
+        alanPara.appendChild(alanParaText);
+        h3Alan.appendChild(alanText);
+        h3Alan.appendChild(alanPara);
+        div3.appendChild(h3Alan);
+
+        header.appendChild(div3);
+        fainPara.appendChild(fainParaText);
+        h3Fain.appendChild(fainText);
+        h3Fain.appendChild(fainPara);
+        div4.appendChild(h3Fain);
+
+        header.appendChild(div4);
+        charlesPara.appendChild(charlesParaText);
+        h3Charles.appendChild(charlesText);
+        h3Charles.appendChild(charlesPara);
+        div5.appendChild(h3Charles);
+        header.appendChild(div5);
+        document.body.appendChild(header);
+    
         for (i = 0; i < friends.length; i++) { 
-        for (let n = 99; n > 0; n--) {            
+            for (let n = 99; n > 0; n--) {            
                 let j = n - 1;
                 if (n === 1) {
                     console.log(+ n + ' line of code in the file,' , + n + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
                 } else {
-                console.log(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');      
+                    console.log(+ n + ' lines of code in the file,', + n +  ' lines of code; ' + friends[i] + ' strikes one out,clears it all out, ' + j + ' lines of code in the file');      
                 }
             }  
         }
     });
 });
+                
+        
+        
 
 
 
